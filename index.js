67d7bb784d62165751dd3c68ac1fa960094d2f02
olyazvk <olga19223@mail.ru>
2021-12-14 01:21:53 +0300

    added block2

--- index.js
+++ index.js
@@ -10,24 +10,44 @@ console.log(flag);
 let txt = 'true';
 console.log(txt);
 
-let x = 5;
-let y = 3;
 
-let a1 = x + y;
+let a1 = 5 + 3;
 console.log(a1);
-let a2 = x - y;
+let a2 = 5 - 3;
 console.log(a2)
-let a3 = x * y;
+let a3 = 5 * 3;
 console.log(a3);
-let a4 = x / y;
+let a4 = 5 / 3;
 console.log(a4);
-let a5 = x % y;
+let a5 = 5 % 3;
 console.log(a5);
-let a6 = y % x;
+
+
+let a6 = 5 % 3;
 console.log(a6);
-let a7 ='3' + x;
+let a7 = 3 % 5;
 console.log(a7);
-let a8 = '5' - y;
+let a8 ='3' + 5;
 console.log(a8);
+let a9 = '5' - 3;
+console.log(a9);
+let a10 = 75 + 'кг';
+console.log(a10);
+
+let width = 10;
+let height = 23;
+let s = width * height + 'см';
+console.log(s);
+
+let heightC =10;
+let pi = 3.14;
+let dC = 4;
+let v = pi * Math.pow(dC,2) / 4 * heightC + ' м.куб';
+console.log(v);
+
+let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(n, 2) + Math.pow(4, 2));
+console.log(k);
 
 
