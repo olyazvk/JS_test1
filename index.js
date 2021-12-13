cd0ead4f4b890e9558f0663bda6425bfaffc12a8
olyazvk <olga19223@mail.ru>
2021-12-14 00:48:51 +0300

    added block1

---
+++ index.js
@@ -0,0 +1,33 @@
+let string = 'Привет';
+console.log(string);
+
+let number = 123;
+console.log(number);
+
+let flag = true;
+console.log(flag);
+
+let txt = 'true';
+console.log(txt);
+
+let x = 5;
+let y = 3;
+
+let a1 = x + y;
+console.log(a1);
+let a2 = x - y;
+console.log(a2)
+let a3 = x * y;
+console.log(a3);
+let a4 = x / y;
+console.log(a4);
+let a5 = x % y;
+console.log(a5);
+let a6 = y % x;
+console.log(a6);
+let a7 ='3' + x;
+console.log(a7);
+let a8 = '5' - y;
+console.log(a8);
+
+
